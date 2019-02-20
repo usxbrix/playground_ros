d0df717f38f761658b5711f9add59792670235b3
Uwe <usx303@googlemail.com>
2019-02-20 15:40:57 +0100

    action client follow

---
+++ src/action-client.py
@@ -0,0 +1,42 @@
+#! /usr/bin/env python
+
+import rospy
+from __future__ import print_function
+
+# Brings in the SimpleActionClient
+import actionlib
+
+# Brings in the messages used by the follow action, including the
+# goal message and the result message.
+import playground_ros.msg
+
+def follow_client():
+    # Creates the SimpleActionClient, passing the type of the action
+    # (FollowAction) to the constructor.
+    client = actionlib.SimpleActionClient('follow', playground_ros.msg.FollowAction)
+
+    # Waits until the action server has started up and started
+    # listening for goals.
+    client.wait_for_server()
+
+    # Creates a goal to send to the action server.
+    goal = playground_ros.msg.FollowGoal(signature=2)
+
+    # Sends the goal to the action server.
+    client.send_goal(goal)
+
+    # Waits for the server to finish performing the action.
+    client.wait_for_result()
+
+    # Prints out the result of executing the action
+    return client.get_result()  # A FollowResult
+
+if __name__ == '__main__':
+    try:
+        # Initializes a rospy node so that the SimpleActionClient can
+        # publish and subscribe over ROS.
+        rospy.init_node('follow_client_py')
+        result = follow_client()
+        print("Result:", ', ', result.outcome)
+    except rospy.ROSInterruptException:
+        print("program interrupted before completion", file=sys.stderr)
